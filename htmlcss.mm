2e6443201b76f1d162f2998086c7073b56b24c2c
inu <vr1090@gmail.com>
2023-01-28 07:03:52 +0700

    add mm

---
+++ htmlcss.mm
@@ -0,0 +1,8 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1674855648576" ID="ID_552481851" MODIFIED="1674864133273" TEXT="html/css">
+<node CREATED="1674864133573" ID="ID_237932824" MODIFIED="1674864142550" POSITION="right" TEXT="sup">
+<node CREATED="1674864178480" ID="ID_1660382295" MODIFIED="1674864180135" TEXT="sub"/>
+</node>
+</node>
+</map>
